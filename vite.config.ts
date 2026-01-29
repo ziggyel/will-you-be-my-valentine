53d367f4841ff8fd3829b4bf853cca4799624ea5
ziggyel <ayelenluque006@gmail.com>
2026-01-29 00:11:56 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine/",
   plugins: [react()],
-});
+  base: '/will-you-be-my-valentine/',
+})
